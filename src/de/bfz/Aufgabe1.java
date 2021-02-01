2171741032ee53292c8d83b41134f2fe24d3f9f4
Christian Richter <Christian.Richter1987@gmail.com>
2021-02-01 13:25:02 +0100

    Add Alternative Wege

--- src/de/bfz/Aufgabe1.java
+++ src/de/bfz/Aufgabe1.java
@@ -32,7 +32,7 @@ public class Aufgabe1 {
 
 
         // Alternativer weg mit zwei Schleifen
-        System.out.println("Alternativer weg zwei Schleifen");
+        System.out.println("Alternativer weg mit zwei Schleifen");
         for (int i = 1; i <= 3; i = i + 1) {
             for (int j = 1; j <= i; j = j + 1) {
                 System.out.print("*");
@@ -57,6 +57,25 @@ public class Aufgabe1 {
                 System.out.println(" *** ");
             }
             if(i == 4){
+                System.out.println("  *  ");
+            }
+        }
+        System.out.println();
+
+        // Alternativer weg mit noch mehr Schleifen
+        System.out.println("Alternativer weg mit noch mehr Schleifen");
+        for(int i = 0; i < 1; i = i + 1) {
+            System.out.println("  *  ");
+            for (int j = 0; j < 1; j = j + 1) {
+                System.out.println(" *** ");
+                for (int k = 0; k < 1; k = k + 1) {
+                    System.out.println("*****");
+                }
+            }
+        }
+        for(int i = 0; i < 1; i = i + 1){
+            System.out.println(" *** ");
+            for(int j = 0; j < 1; j = j + 1){
                 System.out.println("  *  ");
             }
         }
