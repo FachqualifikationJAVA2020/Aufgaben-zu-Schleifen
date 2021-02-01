2896a0feec27187e3485b06b81f07c18438a0f0a
Christian Richter <Christian.Richter1987@gmail.com>
2021-02-01 14:44:08 +0100

    Korrektur

--- src/de/bfz/Aufgabe2.java
+++ src/de/bfz/Aufgabe2.java
@@ -4,10 +4,9 @@ public class Aufgabe2 {
 
     public static void main(String[] args) {
 
-        String piep = "piep";
-        for(int i = 1; i < 21; i = i + 1){
+        for(int i = 1; i < 20; i = i + 1){
             if(i % 3 == 0){
-                System.out.println(piep);
+                System.out.println("piep");
             }else{
                 System.out.println(i);
             }
